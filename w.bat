b59c52a6537e5b45614913469f133c5feea1c32a
Tasnádi Gábor <gabor.tasnadi@evosoft.com>
2022-05-18 22:03:29 +0200

    first commit

---
+++ w.bat
@@ -0,0 +1,7 @@
+@echo off
+%~dp0\miktex\texmfs\install\miktex\bin\x64\pdflatex.exe -synctex=1 -interaction=nonstopmode %~dp0\cv_hu.tex
+%~dp0\miktex\texmfs\install\miktex\bin\x64\biber.exe %~dp0\cv_hu
+%~dp0\miktex\texmfs\install\miktex\bin\x64\pdflatex.exe -synctex=1 -interaction=nonstopmode %~dp0\cv_hu.tex
+%~dp0\miktex\texmfs\install\miktex\bin\x64\pdflatex.exe -synctex=1 -interaction=nonstopmode %~dp0\cv_hu.tex
+
+del *.log *.aux *.bbl *.xml *.gz *.out *.blg *.bcf *.xmpi
